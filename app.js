3fbe2693340cde83863c88563b62970874d169b9
kassimadsen <madsenkassi@gmail.com>
2024-03-30 20:39:42 -0600

    added button active action on click

---
+++ app.js
@@ -0,0 +1,39 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controlls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function PageTransitions() {
+  // make buttons active on click
+  sectBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      e.currentTarget.className += " active-btn";
+    });
+  });
+
+  // make sections active
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      // hide the other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
+}
+
+PageTransitions();
